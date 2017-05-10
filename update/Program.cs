aa42a077779a943d8e50010f797f7ca8a4c0104a
Christoph Stiller <cstiller@euclideon.com>
2017-05-10 15:48:44 +1000

    Added more possible arguments

--- update/Program.cs
+++ update/Program.cs
@@ -7,11 +7,27 @@ using System.IO;
 
 namespace update
 {
-  class Program
+  static class Program
   {
+    static string ToPath(this string path)
+    {
+      if (path.Contains('\\'))
+        path = CurrentDirectory.Replace('\\', '/');
+
+      if (!path.EndsWith("/"))
+        path += "/";
+
+      return path;
+    }
+
     enum EUpdateMode
     {
       update, revert, no_new_files
+    }
+
+    enum OtherArgs
+    {
+      init, info, change, copy
     }
 
     static string CurrentDirectory = null;
@@ -20,25 +36,124 @@ namespace update
     static ulong totalBytes = 0;
     static bool diff = false;
 
+    
     static void Main(string[] args)
     {
       if(args.Length != 0)
       {
-        if (args[0].ToLower() == EUpdateMode.update.ToString())
+        string arg = args[0].ToLower();
+
+        if (arg == EUpdateMode.update.ToString())
         {
           UpdateMode = EUpdateMode.update;
         }
-        else if (args[0].ToLower() == EUpdateMode.revert.ToString())
+        else if (arg == EUpdateMode.revert.ToString())
         {
           UpdateMode = EUpdateMode.revert;
         }
-        else if (args[0].ToLower() == EUpdateMode.no_new_files.ToString())
+        else if (arg == EUpdateMode.no_new_files.ToString())
         {
           UpdateMode = EUpdateMode.no_new_files;
         }
+        else if(arg == OtherArgs.info.ToString())
+        {
+          string currentDirectory = FindDirectoryUpwards(Environment.CurrentDirectory);
+
+          if(currentDirectory == null)
+          {
+            Console.WriteLine("No .update file found.");
+            Environment.Exit(-20);
+          }
+          else
+          {
+            Console.WriteLine($"The current directory is {currentDirectory}.");
+          }
+          return;
+        }
+        else if (arg == OtherArgs.change.ToString())
+        {
+          string directory;
+
+          if (args.Length < 2)
+          {
+            Console.WriteLine("Enter the path to update form");
+            directory = Console.ReadLine().ToPath();
+          }
+          else
+          {
+            directory = args[1].ToPath();
+          }
+
+          if (File.Exists(Environment.CurrentDirectory.ToPath() + ".update"))
+          {
+            Console.WriteLine("Removing original .update file of this directory.");
+            File.Delete(Environment.CurrentDirectory.ToPath() + ".update");
+          }
+
+          CreateUpdateFile(Environment.CurrentDirectory, directory);
+
+          return;
+        }
+        else if (arg == OtherArgs.init.ToString())
+        {
+          string directory;
+
+          if (args.Length < 2)
+          {
+            Console.WriteLine("Enter the path to update form");
+            directory = Console.ReadLine().ToPath();
+          }
+          else
+          {
+            directory = args[1].ToPath();
+          }
+
+          if (File.Exists(Environment.CurrentDirectory.ToPath() + ".update"))
+          {
+            Console.WriteLine("This directory already contains a .update file.\nAborting.");
+            Environment.Exit(-21);
+            return;
+          }
+
+          CreateUpdateFile(Environment.CurrentDirectory, directory);
+
+          return;
+        }
+        else if (arg == OtherArgs.copy.ToString())
+        {
+          if(args.Length > 2)
+          {
+            string arg_ = args[2].ToLower().ToPath();
+            
+            if (arg_ == EUpdateMode.update.ToString())
+            {
+              UpdateMode = EUpdateMode.update;
+            }
+            else if (arg_ == EUpdateMode.revert.ToString())
+            {
+              UpdateMode = EUpdateMode.revert;
+            }
+            else if (arg_ == EUpdateMode.no_new_files.ToString())
+            {
+              UpdateMode = EUpdateMode.no_new_files;
+            }
+            else
+            {
+              Console.WriteLine($"Invalid Update mode '{arg_}'.\nAborting.");
+              Environment.Exit(-22);
+              return;
+            }
+          }
+
+          string srcDirectory = args[1].Trim('"');
+
+          UpdateDirectory(Environment.CurrentDirectory, srcDirectory);
+
+          return;
+        }
         else
         {
-          Console.WriteLine($"Possible Arguments:\nupdate ({EUpdateMode.update},{EUpdateMode.revert},{EUpdateMode.no_new_files}) (diff)\n");
+          Console.WriteLine($"Possible Arguments:\nupdate ({EUpdateMode.update},{EUpdateMode.revert},{EUpdateMode.no_new_files}) (diff)\nupdate {OtherArgs.init} <path>\nupdate {OtherArgs.change} <path>\nupdate {OtherArgs.copy} <source path>\nupdate {OtherArgs.info}");
           return;
         }
 
@@ -46,6 +161,8 @@ namespace update
           if (args[1] == nameof(diff))
             diff = true;
       }
+
+      BEGIN:
 
       try
       {
@@ -71,106 +188,109 @@ namespace update
           Environment.Exit(-3);
         }
 
-        string[] contents;
-
-        try
-        {
-          contents = File.ReadAllLines(CurrentDirectory + ".update");
-        }
-        catch (Exception e)
-        {
-          Console.WriteLine($"An error occured when reading the .update file:\n{e}\n\nAborting.");
-          Environment.Exit(-4);
-          return; // just for compiler errors ;)
-        }
-
-        if (contents.Length == 0)
-        {
-          Console.WriteLine($"An error occured when reading the .update file:\nThe length was 0.\n\nAborting.");
-          Environment.Exit(-5);
-          return; // just for compiler errors ;)
-        }
-
-        if (!Directory.Exists(contents[0]))
-        {
-          Console.WriteLine($"An error occured when reading the .update file:\nThe given directory does not exist or is not accessable.\n\nAborting.");
-          Environment.Exit(-6);
-          return; // just for compiler errors ;)
-        }
-
+        string[] contents = ReadUpdateFile(CurrentDirectory);
         string updateDirectory = contents[0];
 
-        if (updateDirectory.Contains('\\'))
-          updateDirectory = updateDirectory.Replace('\\', '/');
-
-        if (!updateDirectory.EndsWith("/"))
-          updateDirectory += "/";
-
-        if (CurrentDirectory.Contains('\\'))
-          CurrentDirectory = CurrentDirectory.Replace('\\', '/');
-
-        if (!CurrentDirectory.EndsWith("/"))
-          CurrentDirectory += "/";
-
-        string[] updateFiles;
-        List<string> currentFiles;
-
-        try
-        {
-          Console.WriteLine("Reading Current...");
-          currentFiles = Directory.GetFiles(CurrentDirectory, "*", SearchOption.AllDirectories).OrderByDescending(s => s).ToList();
-        }
-        catch (Exception e)
-        {
-          Console.WriteLine($"An error occured when reading the current Directory:\n{e}\n\nAborting.");
-          Environment.Exit(-7);
-          return; // just for compiler errors ;)
-        }
-
-        try
-        {
-          Console.WriteLine("Reading Update...");
-          updateFiles = Directory.GetFiles(updateDirectory, "*", SearchOption.AllDirectories).OrderByDescending(s => s).ToArray();
-        }
-        catch (Exception e)
-        {
-          Console.WriteLine($"An error occured when reading the update Directory:\n{e}\n\nAborting.");
-          Environment.Exit(-8);
-          return; // just for compiler errors ;)
-        }
-
-        Console.WriteLine("Processing files...");
-
-        for (int i = 0; i < updateFiles.Length; i++)
-          updateFiles[i] = updateFiles[i].Substring(updateDirectory.Length).Replace('\\', '/');
-
-        for (int i = 0; i < currentFiles.Count; i++)
-          currentFiles[i] = currentFiles[i].Substring(CurrentDirectory.Length).Replace('\\', '/');
-
-        Console.WriteLine("Updating files...");
-
-        switch(UpdateMode)
-        {
-          case EUpdateMode.revert:
-            UpdateRevert(updateFiles, ref currentFiles, updateDirectory);
-            break;
-
-          case EUpdateMode.update:
-            UpdateUpdate(updateFiles, ref currentFiles, updateDirectory);
-            break;
-
-          case EUpdateMode.no_new_files:
-            UpdateNoNewFiles(updateFiles, ref currentFiles, updateDirectory);
-            break;
-
-          default:
-            Console.WriteLine("Invalid EUpdateMode.\nAborting.");
-            Environment.Exit(-10);
-            return;
-        }
-
-        Console.WriteLine($"Done.\nUpdated {totalFiles} Files ({totalBytes} Bytes).");
+        UpdateDirectory(CurrentDirectory, updateDirectory);
       }
+    }
+
+    private static void UpdateDirectory(string currentDirectory, string updateDirectory)
+    {
+      updateDirectory = updateDirectory.ToPath();
+      currentDirectory = currentDirectory.ToPath();
+
+      string[] updateFiles;
+      List<string> currentFiles;
+
+      try
+      {
+        Console.WriteLine("Reading Current...");
+        currentFiles = Directory.GetFiles(currentDirectory, "*", SearchOption.AllDirectories).OrderByDescending(s => s).ToList();
+      }
+      catch (Exception e)
+      {
+        Console.WriteLine($"An error occured when reading the current Directory:\n{e}\n\nAborting.");
+        Environment.Exit(-7);
+        return; // just for compiler errors ;)
+      }
+
+      try
+      {
+        Console.WriteLine("Reading Update...");
+        updateFiles = Directory.GetFiles(updateDirectory, "*", SearchOption.AllDirectories).OrderByDescending(s => s).ToArray();
+      }
+      catch (Exception e)
+      {
+        Console.WriteLine($"An error occured when reading the update Directory:\n{e}\n\nAborting.");
+        Environment.Exit(-8);
+        return; // just for compiler errors ;)
+      }
+
+      Console.WriteLine("Processing files...");
+
+      for (int i = 0; i < updateFiles.Length; i++)
+        updateFiles[i] = updateFiles[i].Substring(updateDirectory.Length).Replace('\\', '/');
+
+      for (int i = 0; i < currentFiles.Count; i++)
+        currentFiles[i] = currentFiles[i].Substring(currentDirectory.Length).Replace('\\', '/');
+
+      Console.WriteLine("Updating files...");
+
+      switch (UpdateMode)
+      {
+        case EUpdateMode.revert:
+          UpdateRevert(updateFiles, ref currentFiles, updateDirectory);
+          break;
+
+        case EUpdateMode.update:
+          UpdateUpdate(updateFiles, ref currentFiles, updateDirectory);
+          break;
+
+        case EUpdateMode.no_new_files:
+          UpdateNoNewFiles(updateFiles, ref currentFiles, updateDirectory);
+          break;
+
+        default:
+          Console.WriteLine("Invalid EUpdateMode.\nAborting.");
+          Environment.Exit(-10);
+          return;
+      }
+
+      Console.WriteLine($"Done.\nUpdated {totalFiles} Files ({totalBytes} Bytes).");
+    }
+
+
+    private static string[] ReadUpdateFile(string currentDirectory)
+    {
+      string[] ret = null;
+
+      try
+      {
+        ret = File.ReadAllLines(currentDirectory + ".update");
+      }
+      catch (Exception e)
+      {
+        Console.WriteLine($"An error occured when reading the .update file:\n{e}\n\nAborting.");
+        Environment.Exit(-4);
+        throw new Exception(); // just for compiler errors ;)
+      }
+
+      if (ret.Length == 0)
+      {
+        Console.WriteLine($"An error occured when reading the .update file:\nThe length was 0.\n\nAborting.");
+        Environment.Exit(-5);
+        throw new Exception(); // just for compiler errors ;)
+      }
+
+      if (!Directory.Exists(ret[0]))
+      {
+        Console.WriteLine($"An error occured when reading the .update file:\nThe given directory does not exist or is not accessable.\n\nAborting.");
+        Environment.Exit(-6);
+        throw new Exception(); // just for compiler errors ;)
+      }
+
+      return ret;
     }
 
     private static void UpdateRevert(string[] updateFiles, ref List<string> currentFiles, string updateDirectory)
@@ -393,22 +513,16 @@ namespace update
     }
 
 
-    private static void CreateUpdateFile(string currentDirectory)
+    private static void CreateUpdateFile(string currentDirectory, string updateDirectory = null)
     {
-      Console.WriteLine("Enter directory to update from:");
-      string updateDirectory = Console.ReadLine();
+      if (updateDirectory == null)
+      {
+        Console.WriteLine("Enter directory to update from:");
+        updateDirectory = Console.ReadLine();
+      }
 
-      if (currentDirectory.Contains('\\'))
-        currentDirectory = currentDirectory.Replace('\\', '/');
-
-      if (!currentDirectory.EndsWith("/"))
-        currentDirectory += "/";
-
-      if (updateDirectory.Contains('\\'))
-        updateDirectory = updateDirectory.Replace('\\', '/');
-
-      if (!updateDirectory.EndsWith("/"))
-        updateDirectory += "/";
+      updateDirectory = updateDirectory.ToPath();
+      currentDirectory = currentDirectory.ToPath();
 
       if (!Directory.Exists(updateDirectory))
       {
